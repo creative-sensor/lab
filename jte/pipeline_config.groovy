3c62491dbad80dc1cbf12a6e3f267603d6fba65f
Creativ <vietbao.telescope@gmail.com>
2022-04-24 16:18:31 +0700

    fix

--- jte/pipeline_config.groovy
+++ jte/pipeline_config.groovy
@@ -4,12 +4,8 @@
     these should be configurations common across 
     all apps governed by this config. 
 */
-jte {
-    reverse_library_resolution = true
-}
 libraries {
-  override = true 
   //sonarqube
-   maven {
+   @override maven {
    }
 }
