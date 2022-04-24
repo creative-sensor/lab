a659cb981bf8ec378ce36177088ac440e78129ed
Creativ <vietbao.telescope@gmail.com>
2022-04-24 16:31:12 +0700

    fix

--- jte/pipeline_config.groovy
+++ jte/pipeline_config.groovy
@@ -4,6 +4,10 @@
     these should be configurations common across 
     all apps governed by this config. 
 */
+
+jte {
+    permissive_initialization = true
+}
 libraries {
   //sonarqube
    @override maven {
