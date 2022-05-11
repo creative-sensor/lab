fd8158f21d9082f038231a8067848df324e54e6b
Creativ <vietbao.telescope@gmail.com>
2022-05-11 07:33:20 +0700

    fix

--- jte/pipeline_config.groovy
+++ jte/pipeline_config.groovy
@@ -1,20 +1,5 @@
-/*
-  specify which libraries to load: 
-    In the Governance Tier configuration file, 
-    these should be configurations common across 
-    all apps governed by this config. 
-*/
 
-jte {
-    permissive_initialization = true
-}
 libraries {
-  //sonarqube
-    maven {
-        @override build{}
-   }
+    maven
 }
 
-template_methods{
-  build
-}
