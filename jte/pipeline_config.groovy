ed0c43cc2aaf98a39fdea03362aa172c954b806a
Creativ <vietbao.telescope@gmail.com>
2022-05-17 22:52:46 +0700

    dka;senk

--- jte/pipeline_config.groovy
+++ jte/pipeline_config.groovy
@@ -2,6 +2,7 @@ jte{
   permissive_initialization = true // pipeline will fail if not set
   //reverse_library_resolution = true
   allow_scm_jenkinsfile = false
+  reverse_library_resolution = true
 }
 
 libraries {
