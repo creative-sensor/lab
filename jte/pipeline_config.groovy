b05f80838507c959c8a21cb4bcd7b0336350472d
Creativ <vietbao.telescope@gmail.com>
2022-05-17 22:15:21 +0700

    fix

--- jte/pipeline_config.groovy
+++ jte/pipeline_config.groovy
@@ -11,7 +11,12 @@ libraries {
 
 stages {
     creativ{
-        jte.libraries.maven.build_mute
+        maven {
+            build_mute
+        }
+        rhinos {
+            build_mute
+        }
     }
 //    elite {
 //        jte.libraries.rhinos.build_mute
